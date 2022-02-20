210c12559355f32a35c4f13cd3cbf7985160e439
shanthiniguru <96715415+shanthiniguru@users.noreply.github.com>
2022-02-20 13:19:05 +0530

    Update Factorial.java

--- week1/day1/classroom/Factorial.java
+++ week1/day1/classroom/Factorial.java
@@ -22,21 +22,28 @@ public class Factorial {
 	public static void main(String[] args) {
 
 		// Declare your input as 5
+		int input = 5;
 		
 		
 		// Declare an integer variable fact as 1
+		int fact = 1;
 		
 
 		// Iterate from 1 to your input (tip: using loop concept)
+		for (int i = 1; i <= input; i++){
+		
+			
+		
 		
 
 			// Within the loop: Multiply fact with the iterator variable (Tip: Assign it to the 'fact' variable)
-			
+				fact = i * fact;
 
 		//End of loop
 		
-
+}
 		// Print factorial (fact)
+		System.out.println(fact);	
 	
 	}
 
